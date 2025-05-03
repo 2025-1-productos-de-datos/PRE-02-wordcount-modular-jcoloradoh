cceb4f1e27c11c8fcd643ad6864dbdb39d367034
jcoloradoh <jcoloradoh@unal.edu.co>
2025-05-03 10:50:06 -0500

    Move_file_to_internals

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,43 +1,38 @@
 # obtain a list of files in the input directory
-import os
 
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_in_words import split_in_words
 from ._internals.write_count_words import write_count_words
-
-
-def read_all_lines():
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for filename in input_file_list:
-        with open(filename, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(lines)
-    return all_lines
 
 
 def main():
 
-    ##
-    input_file_list = os.listdir("data/input/")
-    # all_lines = read_all_lines()
+    ## mover a la funcion "read_all_lines"
+    all_lines = read_all_lines()
 
-    ## read all lines
-    ## preprocess lines
-    ## split in words
-    ## count words
-    ## write count words
+    ## mover a "preprocess_lines"
+    all_lines = preprocess_lines(all_lines)
+
+    ## mover "split_in_words"
+    words = split_in_words(all_lines)
+
+    ## mover a "count_words"
+    counter = count_words(words)
 
     # count the frequency of the words in the files in the input directory
-    counter = {}
-    for filename in input_file_list:
-        with open("data/input/" + filename) as f:
-            for l in f:
-                for w in l.split():
-                    w = w.lower().strip(",.!?")
-                    counter[w] = counter.get(w, 0) + 1
+    # counter = {}
+    # for filename in input_file_list:
+    #     with open("data/input/" + filename) as f:
+    #         for l in f:
+    #             for w in l.split():
+    #                 w = w.lower().strip(",.!?")
+    #                 counter[w] = counter.get(w, 0) + 1
 
+    ##
     write_count_words(counter)
 
 
-# esto solamente se ejecuta cuando se ingresa en terminal python main.py
 if __name__ == "__main__":
     main()
